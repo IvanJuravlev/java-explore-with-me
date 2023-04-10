ef7d29ab33b79b231460e3550b5f10ec14f049c5
MrLeftwing <ivanjuravlev2014@gmail.com>
2023-04-10 23:37:31 +0300

    Добавил сущность пользователя

---
+++ ewm-main/src/main/java/ru/practicum/model/Category.java
@@ -0,0 +1,20 @@
+package ru.practicum.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+}
---
+++ ewm-main/src/main/java/ru/practicum/model/Event.java
@@ -0,0 +1,60 @@
+package ru.practicum.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "EVENTS")
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String annotation;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    private String description;
+
+    @Column(name = "event_date")
+    private LocalDateTime eventDate;
+
+    @ManyToOne
+    @JoinColumn(name = "initiator_id")
+    private User initiator;
+
+    @ManyToOne
+    @JoinColumn(name = "location_id")
+    private Location location;
+
+    private Boolean paid;
+
+    @Column(name = "participant_limit")
+    private Integer participantLimit;
+
+    @Column(name = "published_on")
+    private LocalDateTime publishedOn;
+
+    @Column(name = "request_moderation")
+    private Boolean requestModeration;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "event_state")
+    private EventState eventState;
+
+    private String title;
+}
+}
